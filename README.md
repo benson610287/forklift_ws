df0779b0595196ac49ed9e5008acdc427afa60ae
jackyoyo564 <aa0918662361@gmail.com>
2025-03-25 20:48:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# timda-mobile
+
+2020 Designed
+![Timda Mobile](https://i.imgur.com/93NvHtg.png)
+
+## Hardwares Description
+| Item          	| Item Model 	| Vendor        	| Description                                                                         	|
+|---------------	|------------	|---------------	|-------------------------------------------------------------------------------------	|
+| Motor         	| BLVM620KM  	| Orientalmotor 	| Weight: 6kg<br> Power: 200W<br> Constant Torque: 0.65Nm<br> Constant Speed: 3000rpm 	|
+| Controller    	| BLVD20KM   	| Orientalmotor 	|                                                                                     	|
+| Mecanum wheel 	|            	|               	| 6 inch
+
+## Systems
+OS: Ubuntu 18.04 bionic
+ROS: noetic
+
+## Installization and Setup
+### Clone this repo and submodules
+```base
+$ cd $HOME
+$ mkdir -p timda_mobile_ws/src && cd timda_mobile_ws
+$ git clone https://github.com/benson610287/timda_mobile_noetic.git src/ --recursive
+```
+### Requirements
+ROS Dependments:
+```bash
+$ sudo apt-get install ros-noetic-hector-sensors-description
+$ sudo apt-get install ros-noetic-rosbridge-server ros-noetic-hector-slam ros-noetic-amcl ros-noetic-move-base ros-noetic-dwa-local-planner ros-noetic-map-server ros-noetic-teb-local-planner
+## Hokuyo UTM30LX driver
+$ sudo apt-get install ros-noetic-urg-node
+```
+
+Controller needs `libbmodbus`, please check out [mecanum](/src/mecanum)
+
+Setup WoL client, we use raspberry pi 3 to send WoL signal to Intel NUC. Please check out [ping-pong client](/src/scripts) if needs.
+
+There are udev files for hardware devices like Hokuyo lidar or RS485 adapter. Please check out [udev_rules](/src/udev_rules) and [systemd](/src/systemd) if needs.
+
+We use nodeJS to setup http server. Please check out [webserver](/src/webserver) to install dependencies.
+
+### Build
+```base
+$ cd $HOME/timda_mobile_ws
+$ catkin_make
+```
---
+++ src/timda_mobile_humble/src/mecanum/src/mobile_node.cpp
@@ -0,0 +1,217 @@
+#include "rclcpp/rclcpp.hpp"
+#include <geometry_msgs/msg/twist.hpp>
+#include <iostream>
+#include <iterator>
+#include <mecanum/Mecanum.h>
+#include <mecanum/TimdaModbus.h>
+// #include <tf/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <nav_msgs/msg/odometry.hpp>
+#include <pthread.h>
+
+#include "geometry_msgs/msg/quaternion.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"  // 包含 tf2_geometry_msgs 头文件
+
+
+#define K 0.10472                 // 2 * pi / 60
+#define MAX_RPM 3979
+#define MIN_RPM 83
+
+typedef void * (*THREADFUNCPTR)(void *);
+
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+class TimdaMobile : public rclcpp::Node
+{
+    // ros1
+    // ros::NodeHandle nh_;: pub: public rclcpp::Nodelic rclcpp::Nod: public rclcpp::Nodee
+    // ros::Subscriber sub_;
+    // ros::Publisher odom_pub;
+    // tf::TransformBroadcaster odom_broadcaster;
+    // ros::Rate *loop_rate;
+    // ros::Time current_time, last_time;  
+    // ros2
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_;
+    // tf2_ros::TransformBroadcaster odom_broadcaster;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> odom_broadcaster;
+
+    // rclcpp::Rate *loop_rate;
+    std::shared_ptr<rclcpp::Rate> loop_rate;
+    rclcpp::Time current_time, last_time;
+    
+
+    pthread_t t;
+    Mecanum *M;
+    TimdaModbus *TM;
+    double a = 0.24145;
+    double b = 0.253;
+    double R = 0.15098;
+    double rx = 0.0;
+    double ry = 0.0;
+    double rz = 0.0;
+    double reduction = 30;
+
+private:
+    double RPM;
+
+public:
+    TimdaMobile() : Node("mobile_node")
+    {
+        // ros1
+        // sub_ = nh_.subscribe("mobile/cmd_vel", 1, &TimdaMobile::Callback, this);
+        // odom_pub = nh_.advertise<nav_msgs::Odometry>("odom", 50);
+        // loop_rate = new ros::Rate(1000);
+        // current_time = ros::Time::now();
+        // last_time = ros::Time::now();
+        // ros2
+        // rclcpp::Node::SharedPtr node(this);
+        odom_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+
+        sub_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&TimdaMobile::Callback, this, std::placeholders::_1));
+        odom_pub = this->create_publisher<nav_msgs::msg::Odometry>("odom", 50);
+        loop_rate = std::make_shared<rclcpp::Rate>(1000);
+        current_time = this->now();
+        last_time = this->now();
+
+
+        M = new Mecanum(this->a, this->b, this->R);
+        TM = new TimdaModbus();
+
+        Looper();
+    }
+
+    ~TimdaMobile()
+    {
+        delete M;
+        delete TM;
+        // delete loop_rate;
+    }
+
+    void Looper()
+    {
+        while (rclcpp::ok()) {
+            std::vector<int> MRPM(4, 0);
+            std::vector<double> V(3, 0);
+            MRPM = TM->read_motor_rpm();
+            // std::cout<<"Motor's RPM:"<<MRPM.at(0)<<" "
+            //                          <<MRPM.at(1)<<" "
+            //                          <<MRPM.at(2)<<" "
+            //                          <<MRPM.at(3)<<std::endl;
+            V = M->FK(RPM2W(MRPM.at(0)), RPM2W(MRPM.at(1))*-1,
+                      RPM2W(MRPM.at(2)), RPM2W(MRPM.at(3))*-1);
+            // std::cout<<"Robot's Speed:"<<V.at(0)<<" "
+            //                            <<V.at(1)<<" "
+            //                            <<V.at(2)<<std::endl;
+            current_time =this->now();
+            double dt = (current_time - last_time).seconds();
+            double delta_x = (V.at(0)*cos(rz) - V.at(1)*sin(rz)) * dt;
+            double delta_y = (V.at(0)*sin(rz) + V.at(1)*cos(rz)) * dt;
+            double delta_th = V.at(2)* dt;
+
+            rx += delta_x;
+            ry += delta_y;
+            rz += delta_th;
+            // std::cout<<"Robot's Position: "<<rx<<" "
+            //                                <<ry<<" "
+            //                                <<rz<<" "
+            //                                <<rz*(180.0/M_PI)<<std::endl;
+
+            //since all odometry is 6DOF we'll need a quaternion created from yaw
+            geometry_msgs::msg::Quaternion odom_quat = tf2::toMsg(tf2::Quaternion(tf2::Vector3(0, 0, 1), rz));
+
+            //first, we'll publish the transform over tf
+            geometry_msgs::msg::TransformStamped odom_trans;
+            odom_trans.header.stamp = current_time;
+            odom_trans.header.frame_id = "odom";
+            odom_trans.child_frame_id = "base_link";
+
+            odom_trans.transform.translation.x = rx;
+            odom_trans.transform.translation.y = ry;
+            odom_trans.transform.translation.z = 0.0;
+            odom_trans.transform.rotation = odom_quat;
+
+            //send the transform
+            odom_broadcaster->sendTransform(odom_trans);
+
+            //next, we'll publish the odometry message over ROS
+            nav_msgs::msg::Odometry odom;
+            odom.header.stamp = current_time;
+            odom.header.frame_id = "odom";
+
+            //set the position
+            odom.pose.pose.position.x = rx;
+            odom.pose.pose.position.y = ry;
+            odom.pose.pose.position.z = 0.0;
+            odom.pose.pose.orientation = odom_quat;
+
+            //set the velocity
+            odom.child_frame_id = "base_link";
+            odom.twist.twist.linear.x = V.at(0);
+            odom.twist.twist.linear.y = V.at(1);
+            odom.twist.twist.angular.z = V.at(2);
+
+            //publish the message
+            odom_pub->publish(odom);
+
+            last_time = current_time;
+            // rclcpp::spin();
+            // rclcpp::spin_some(shared_from_this());
+
+            rclcpp::spin_some(this->get_node_base_interface());
+            // loop_rate->sleep();
+        }
+    }
+
+    void Callback(const geometry_msgs::msg::Twist & msg)
+    {
+        std::vector<double> W(4, 0);
+        W = M->IK(msg.linear.x,
+                  msg.linear.y,
+                  msg.angular.z);
+        // std::cout<<"Return from IK:"<<W.at(0)<<" "
+        //                            <<W.at(1)<<" "
+        //                            <<W.at(2)<<" "
+        //                            <<W.at(3)<<std::endl;
+        this->TM->move(W2RPM(W.at(0)), W2RPM(W.at(1)), W2RPM(W.at(2)), W2RPM(W.at(3)));
+        // std::cout<<"Send RPM: "<< W2RPM(W.at(0)) <<" "
+        //                        << W2RPM(W.at(1))<<" "
+        //                        << W2RPM(W.at(2))<<" "
+        //                        << W2RPM(W.at(3))<<std::endl;
+    }
+
+    int W2RPM(double W)
+    {
+        double v = W * this->R;
+        RPM = (v / this->R)/K*reduction;
+        // RPM = (abs(RPM) > MAX_RPM) ? MAX_RPM*sgn(RPM) : RPM;
+        // RPM = (abs(RPM) < MIN_RPM) ? 0 : RPM;
+        // return (int)RPM;
+        /* Re-Mapping Range */
+        double slope = 1.0 * (MAX_RPM - MIN_RPM) / (4627 - 0);
+        double output = MIN_RPM + slope * abs(RPM);
+        return (int)output * sgn(RPM);
+    }
+
+    double RPM2W(int RPM)
+    {
+        return K*RPM/reduction;
+    }
+};
+
+int main(int argc, char **argv)
+{
+//   ros::init(argc, argv, "mobile_node");
+  rclcpp::init(argc, argv);
+  printf("AA");
+//   rclcpp::spin(std::make_shared<TimdaMobile>());
+  printf("AA");
+  TimdaMobile tm;
+
+//   ros::spin();
+
+  return 0;
+}
