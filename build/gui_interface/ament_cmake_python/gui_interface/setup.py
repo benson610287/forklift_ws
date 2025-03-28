fcddb50749d36cf07ddbb0fe39ea69a8b77e00d5
benson <benson610287@gmail.com>
2025-03-28 15:03:55 +0800

    0328

--- build/gui_interface/ament_cmake_python/gui_interface/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='gui_interface',
-    version='0.0.0',
-    packages=find_packages(
-        include=('gui_interface', 'gui_interface.*')),
-)
--- build/main_control/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/fxck/work/install/gui_interface/share/gui_interface/package.sh"
-. "/home/fxck/work/install/interface/share/interface/package.sh"
