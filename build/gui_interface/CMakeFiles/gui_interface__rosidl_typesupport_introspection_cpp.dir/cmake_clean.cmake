699025cba24cc2e9aad2643d758d6431e52e761e
benson <benson610287@gmail.com>
2025-03-28 15:03:36 +0800

    0328

---
+++ build/gui_interface/CMakeFiles/gui_interface__rosidl_typesupport_introspection_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gui_interface__rosidl_typesupport_introspection_cpp.dir/rosidl_typesupport_introspection_cpp/gui_interface/srv/detail/taskcmd__type_support.cpp.o"
+  "CMakeFiles/gui_interface__rosidl_typesupport_introspection_cpp.dir/rosidl_typesupport_introspection_cpp/gui_interface/srv/detail/taskcmd__type_support.cpp.o.d"
+  "libgui_interface__rosidl_typesupport_introspection_cpp.pdb"
+  "libgui_interface__rosidl_typesupport_introspection_cpp.so"
+  "rosidl_typesupport_introspection_cpp/gui_interface/srv/detail/taskcmd__rosidl_typesupport_introspection_cpp.hpp"
+  "rosidl_typesupport_introspection_cpp/gui_interface/srv/detail/taskcmd__type_support.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gui_interface__rosidl_typesupport_introspection_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ install/interface/include/interface/interface/srv/maincontroller.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from interface:srv/Maincontroller.idl
+// generated code does not contain a copyright notice
+
+#ifndef INTERFACE__SRV__MAINCONTROLLER_H_
+#define INTERFACE__SRV__MAINCONTROLLER_H_
+
+#include "interface/srv/detail/maincontroller__struct.h"
+#include "interface/srv/detail/maincontroller__functions.h"
+#include "interface/srv/detail/maincontroller__type_support.h"
+
+#endif  // INTERFACE__SRV__MAINCONTROLLER_H_
