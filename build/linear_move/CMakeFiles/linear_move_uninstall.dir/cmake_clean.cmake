ab535e860eb5db72ae7698cec9b177b58ea61121
benson610287 <benson610287@gmail.com>
2025-04-11 16:46:51 +0800

    test

---
+++ build/linear_move/CMakeFiles/linear_move_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linear_move_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/linear_move_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
