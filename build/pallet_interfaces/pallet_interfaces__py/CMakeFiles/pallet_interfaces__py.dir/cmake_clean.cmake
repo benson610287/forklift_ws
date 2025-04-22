1c58797ef5d2998e2707c3da3a02e43d5bce96f4
taiwenlin <409500559@gms.tku.edu.tw>
2025-04-22 16:07:51 +0800

    getbox width and length v1

---
+++ build/pallet_interfaces/pallet_interfaces__py/CMakeFiles/pallet_interfaces__py.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/_pallet_interfaces_s.ep.rosidl_typesupport_c.c"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/_pallet_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/_pallet_interfaces_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/msg/__init__.py"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/msg/_boxinfo.py"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/msg/_boxinfo_s.c"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/srv/__init__.py"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/srv/_palletstate.py"
+  "/home/aa/work/build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/srv/_palletstate_s.c"
+  "CMakeFiles/pallet_interfaces__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pallet_interfaces__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
