f6c597c6888297d3cc05332d668c55da142a9f4f
glenn901217 <glenn901217@gmail.com>
2025-04-29 17:10:17 +0800

    add yolo

--- build/main_control/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/eating/work/install/gui_interface/share/gui_interface/package.sh"
-. "/home/eating/work/install/interface/share/interface/package.sh"
