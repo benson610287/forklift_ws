d095e81fc9bdea470ce8a43614c636fe36cc297d
benson610287 <benson610287@gmail.com>
2025-05-01 15:58:08 +0800

    merge glenn

--- build/docking_interface/docking_interface__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fxck/work/build/docking_interface/docking_interface__py
-# Build directory: /home/fxck/work/build/docking_interface/docking_interface__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
