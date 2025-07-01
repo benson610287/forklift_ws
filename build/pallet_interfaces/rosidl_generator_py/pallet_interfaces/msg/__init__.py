5a608decc8dc3e319b4074b4d7f357260722a4d6
glenn901217 <glenn901217@gmail.com>
2025-07-01 16:13:06 +0800

    launch file

--- build/pallet_interfaces/rosidl_generator_py/pallet_interfaces/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from pallet_interfaces.msg._boxinfo import Boxinfo  # noqa: F401
--- build/pallet_main/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/eating/work/install/pallet_main'
