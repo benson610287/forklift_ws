133a9e421f826fa575068ad21f63f8f2d1223247
glenn901217 <glenn901217@gmail.com>
2025-07-14 20:45:44 +0800

    OK

--- build/interface/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eating/work/src/interface
-# Build directory: /home/eating/work/build/interface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("interface__py")
